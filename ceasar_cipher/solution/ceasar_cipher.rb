26cf1a2b440033224e2da0ff9cf55b739971ffcc
louisonrails <237607115+louisonrails@users.noreply.github.com>
2025-11-01 10:20:11 -0300

    Wrapping logic with modulo operator and tests - Replaced manual wrapping logic with modulo operator for cleaner and more efficient code. - Implemented RSpec tests to cover:   - Positive shifts, including handling accented characters and wrapping.   - Negative shifts.   - Non-alphabetic characters (spaces and punctuation) preservation.   - Edge cases such as empty strings and large shift values.

--- ceasar_cipher/solution/ceasar_cipher.rb
+++ ceasar_cipher/solution/ceasar_cipher.rb
@@ -1,28 +1,18 @@
 def ceasar_cipher(string, number)
   alphabet = ('a'..'z').to_a
-  max_index = alphabet.index(alphabet.max)
   string_array = string.downcase.split('')
   result = ""
-  
+
   string_array.each do |char|
     if alphabet.include?(char)
       index = alphabet.index(char)
-      new_index = index + number
-      if new_index > max_index
-        difference = new_index - max_index
-        new_char = alphabet[difference]
-      else
-        new_char = alphabet[new_index]
-      end
+      new_index = (index + number) % 26
+      new_char = alphabet[new_index]
       result += new_char
     else
-      result += char
-      
+      result += char 
     end
   end
 
-  puts result
-
+  result
 end
-
-ceasar_cipher("I like Brownies", 5)
